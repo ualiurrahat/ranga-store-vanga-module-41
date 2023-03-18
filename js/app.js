cca6e2aab9a52f2a9ad6611499bae08e4c1de3c5
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-18 22:26:59 +0600

    fixed issues with add to cart,total amount, product quantity

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
@@ -68,16 +68,17 @@ const showProductDetailsInModal = (product_details) => {
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   // document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = parseFloat(total.toFixed(2));
 };
 
 // set innerText function
